24fc48f2ca955b42369e69a87664833316f477c0
颜海镜 <yanhaijing@yeah.net>
2019-03-21 16:15:25 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -9,3 +9,31 @@ export function compose(...fns) {
         return fns.reduceRight((prev, fn) => fn(...[].concat(prev)), args);
     };
 }
+
+export function curry(func, len = func.length) {
+    function partial(func, argsList, argsLen) {
+        if (argsList.length >= argsLen) {
+            return func.apply(this, argsList);
+        }
+
+        return function (...args) {
+            return partial.call(this, func, argsList.concat(args), argsLen);
+        }
+    }
+
+    return partial(func, [], len);
+}
+
+export function curryRight(func, len = func.length) {
+    function partial(func, argsList, argsLen) {
+        if (argsList.length >= argsLen) {
+            return func.apply(this, argsList);
+        }
+
+        return function (...args) {
+            return partial.call(this, func, args.concat(argsList), argsLen);
+        }
+    }
+
+    return partial(func, [], len);
+}
