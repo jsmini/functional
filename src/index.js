8b3791b32609fdd47d275beac8e8b9f968cd0a7e
颜海镜 <yanhaijing@yeah.net>
2019-03-22 13:29:50 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { type } from '@utiljs/type';
+
 export function pipe(...fns) {
     return function (...args) {
         return fns.reduce((prev, fn) => fn(...[].concat(prev)), args);
@@ -36,4 +38,56 @@ export function curryRight(func, len = func.length) {
     }
 
     return partial(func, [], len);
+}
+
+export function debounce(func, time = 0) {
+    if (type(func) !== 'function') {
+        throw new TypeError('debounce first param must be a function');
+    }
+
+    time = Math.ceil(time);
+
+    if (isNaN(time)) {
+        throw new TypeError('debounce second param must be a number');
+    }
+
+    let tid = null;
+    return function debounced(...args) {
+        clearTimeout(tid);
+
+        tid = setTimeout(() => {
+            func.apply(this, args);
+        }, time);
+    }
+}
+
+export function throttle(func, time = 0) {
+    if (type(func) !== 'function') {
+        throw new TypeError('throttle first param must be a function');
+    }
+
+    time = Math.ceil(time);
+
+    if (isNaN(time)) {
+        throw new TypeError('throttle second param must be a number');
+    }
+
+    let tid = null;
+    let lasttime = 0;
+
+    return function throttled(...args) {
+        const nowtime = (new Date).getTime();
+
+        if (nowtime - lasttime >= time) {
+            lasttime = nowtime;
+            func.apply(this, args);
+            return;
+        }
+
+        clearTimeout(tid);
+        tid = setTimeout(() => {
+            lasttime = (new Date).getTime();
+            func.apply(this, args);
+        }, time - (nowtime - lasttime));
+    }
 }
